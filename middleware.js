169219c08b0ce0b488c474fc9f63d5cc5a0e07d3
Leandro Marino <leandromarino@Leandro.Marino>
2016-03-22 23:21:40 -0300

    Modularização do Middleware e web-server funcionando

---
+++ middleware.js
@@ -0,0 +1,14 @@
+//Use module exports to expose middleware
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		//new Date().toString()
+		console.log(req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
+		next();
+	}
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -2,17 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next){
-		//new Date().toString()
-		console.log(req.method + ' ' + req.originalUrl + ' ' + new Date().toString());
-		next();
-	}
-};
+
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
